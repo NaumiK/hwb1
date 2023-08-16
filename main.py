e9aed3f517bab6c03266b695943d448e581e6c3f
NaumikhinDD <daniil.study@yandex.ru>
2023-08-16 17:25:53 +0300

    "Default-dimension"

--- main.py
+++ main.py
@@ -3,18 +3,29 @@ import streamlit as st
 import pandas as pd
 import numpy as np, scipy as sp
 import plotly.express as px
+import plotly.graph_objects as go
 
 class App:
     __numerics = ["int16", "int32", "int64", "float16", "float32", "float64"]
-    plot_functions: list[dict[str: Callable[[pd.DataFrame, tuple[str]],None]]] = [
-            dict(),
-            {
+    def __check_category(df: pd.DataFrame, xy: tuple[str], func: Callable[[pd.DataFrame, tuple[str]], go.Figure]) -> go.Figure | None:
+        if all(map(lambda x: df.dtypes[x] in App.__numerics, xy)):
+            return func(df, xy)
+        else:
+            st.error("Column type must be numeric, check out INFO section")
+            return None
+
+    plot_functions: dict[int: dict[str: Callable[[pd.DataFrame, tuple[str]], go.Figure | None]]] = {
+            # Default 
+            0: {
+                "Corr Heatmap":     lambda df, xy: App.__check_category(df, xy, 
+                                        lambda a, b: px.imshow (a[list(b)].corr())),
+            },
+            2: {
                 "Violin plot x2":   lambda df, xy: px.violin    (df, x=xy[0], y=xy[1]),
                 "Box plot x2":      lambda df, xy: px.box       (df, x=xy[0], y=xy[1]),
-                "Scatter plot":     lambda df, xy: px.scatter   (df, x=xy[0], y=xy[1])
+                "Scatter plot":     lambda df, xy: px.scatter   (df, x=xy[0], y=xy[1]),
             },
-            dict()
-        ]
+        }
 
     def __init__(self) -> None:
         self.dataframe:     pd.DataFrame    = None
@@ -46,23 +57,27 @@ class App:
     def plot(self) -> None:
         st.header("Plot")
         if self.column_type is None: return
-        dimension = st.number_input("Set dimension", 1, 3)
+        dimension = st.number_input("Set dimension", min_value=1)
         cols = self.choose_columns(dimension)
         if cols is not None:
-            type_of_plot = st.selectbox("Type of plot", App.plot_functions[dimension - 1].keys())
+            type_of_plot = st.selectbox("Type of plot", 
+                                        (list(App.plot_functions[dimension].keys()) if dimension in App.plot_functions else []) + 
+                                        list(App.plot_functions[0].keys()))
             draw_plot = st.button("Plot!")
             if draw_plot:
-                fig = App.plot_functions[dimension - 1][type_of_plot](self.dataframe, cols)
-                st.plotly_chart(fig)
+                if dimension in App.plot_functions and type_of_plot in App.plot_functions[dimension]:
+                    fig = App.plot_functions[dimension][type_of_plot](self.dataframe, cols)
+                else:
+                    fig = App.plot_functions[0][type_of_plot](self.dataframe, cols)
+                if fig is not None: st.plotly_chart(fig)
 
     def main(self) -> None:
         st.title("Промежуточная аттестация")
         self.upload_dataframe()
         self.show_info()
         if self.dataframe is not None:
-            plot_agree = st.checkbox("Plot")
-            if plot_agree: self.plot()
-
+            check_show_plot = st.checkbox("Plot")
+            if check_show_plot: self.plot()
 
 if __name__ == "__main__":
     app = App()
